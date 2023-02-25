d352a8a7a8b6ea85b86dec9ff69988b28c1c82f7
shwetabh2002 <101383671+shwetabh2002@users.noreply.github.com>
2023-02-25 16:44:37 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+
+
+import AuthPage from "./AuthPage";
+import ChatsPage from "./ChatsPage";
+
+function App() {
+  const [user, setUser] = useState();
+
+  if (!user) {
+    return <AuthPage onAuth={(user) => setUser(user)} />;
+  } else {
+    return <ChatsPage user={user} />;
+  }
+}
+
+export default App;
